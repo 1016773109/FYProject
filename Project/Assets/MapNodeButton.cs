07d896ddad0923cf7d2b900ad758449c2e3d851e
qiuyu_670604 <10167731092qq.com>
2018-07-20 19:07:16 +0800

    更新

--- Project/Assets/MapNodeButton.cs
+++ Project/Assets/MapNodeButton.cs
@@ -4,22 +4,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MapNodeButton : MonoBehaviour,
-    IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler,
-    IBeginDragHandler, IDragHandler, IEndDragHandler,
-    IMoveHandler, IInitializePotentialDragHandler,
-    ISubmitHandler, ICancelHandler,
-    ISelectHandler, IDeselectHandler,
-    IUpdateSelectedHandler, IScrollHandler, IDropHandler
+public class MapNodeButton : EventTrigger
 {
     private SpriteRenderer sprite;
+    private Color32 NormalColor = new Color32(255, 255, 255, 255);
+    private Color32 HighlightedColor = new Color32(245, 245, 245, 255);
+    private Color32 PressedColor = new Color32(200, 200, 200, 255);
+    private Color32 DisabledColor = new Color32(200, 200, 200, 128);
 
-    private void Awake()
+    private void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
     }
 
-    public void OnPointerClick(PointerEventData eventData)
+
+    void SetColor(Color32 color)
+    {
+        sprite.color = color;
+    }
+
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerEnter");
+        SetColor(HighlightedColor);
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerExit");
+        SetColor(NormalColor);
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+        SetColor(PressedColor);
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerUp");
+        SetColor(HighlightedColor);
+    }
+
+    public override void OnPointerClick(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Left)
         {
@@ -27,116 +55,9 @@ public class MapNodeButton : MonoBehaviour,
         }
     }
 
-    public void OnMove(AxisEventData eventData)
-    {
-        Debug.Log("OnMove");
-    }
-
     void OnMouseOver()
     {
         Debug.Log("OnMouseOver");
     }
 
-    void OnMouseDown()
-    {
-        Debug.Log("OnMouseDown");
-    }
-
-    void OnMouseDrag()
-    {
-        Debug.Log("OnMouseDrag");
-    }
-    void OnMouseEnter()
-    {
-        Debug.Log("OnMouseEnter");
-    }
-
-    void OnMouseExit()
-    {
-        Debug.Log("OnMouseExit");
-    }
-    void OnMouseUp()
-    {
-        Debug.Log("OnMouseUp");
-    }
-
-    void OnMouseUpAsButton()
-    {
-        Debug.Log("OnMouseUpAsButton");
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        Debug.Log("OnPointerDown");
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        Debug.Log("OnPointerUp");
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        Debug.Log("OnDrag");
-    }
-
-    public void OnDrop(PointerEventData eventData)
-    {
-        Debug.Log("OnDrop");
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        Debug.Log("OnPointerEnter");
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        Debug.Log("OnPointerExit");
-    }
-
-    public void OnDeselect(BaseEventData eventData)
-    {
-        Debug.Log("OnDeselect");
-    }
-
-    public void OnScroll(PointerEventData eventData)
-    {
-        Debug.Log("OnScroll");
-    }
-
-    public void OnUpdateSelected(BaseEventData eventData)
-    {
-        Debug.Log("OnUpdateSelected");
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        Debug.Log("OnEndDrag");
-    }
-
-    public void OnInitializePotentialDrag(PointerEventData eventData)
-    {
-        Debug.Log("OnInitializePotentialDrag");
-    }
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        Debug.Log("OnBeginDrag");
-    }
-
-    public void OnSubmit(BaseEventData eventData)
-    {
-        Debug.Log("OnSubmit");
-    }
-
-    public void OnCancel(BaseEventData eventData)
-    {
-        Debug.Log("OnCancel");
-    }
-
-    public void OnSelect(BaseEventData eventData)
-    {
-        Debug.Log("OnSelect");
-    }
 }
